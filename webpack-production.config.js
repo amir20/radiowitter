65fc040b587c7280eda54416a366b039bfd7f693
Amir Raminfar <amir.raminfar@opower.com>
2015-11-02 17:07:27 -0800

    Adds webpack build

--- webpack-production.config.js
+++ webpack-production.config.js
@@ -11,9 +11,12 @@ module.exports = {
     module: {
         loaders: [
             {
-                test: /\.jsx?$/, exclude: /node_modules/, loader: 'babel-loader', query: {
-                presets: ['es2015', 'react']
-            }
+                test: /\.jsx?$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader',
+                query: {
+                    presets: ['es2015', 'react']
+                }
             }
         ]
     },
